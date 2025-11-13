4f72c4939409516e2066ebdc6743b010dcd2b5dc
shoxxdj <dj.shoxx.dj@gmail.com>
2025-11-13 12:29:25 +0100

    Update aws.sh

--- CLOUDPWN/aws.sh
+++ CLOUDPWN/aws.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/hostname
 curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/iam/info
